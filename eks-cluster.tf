4a72a5b58e07fdeaf8c1a8a878fe50f00e9bd741
sharma-raj <rajendrasharma2907@gmail.com>
2021-12-21 14:35:00 +0530

    Create eks-cluster.tf

---
+++ eks-cluster.tf
@@ -0,0 +1,43 @@
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = local.cluster_name
+  cluster_version = "1.20"
+  subnets         = module.vpc.private_subnets
+
+  tags = {
+    Environment = "training"
+    GithubRepo  = "terraform-aws-eks"
+    GithubOrg   = "terraform-aws-modules"
+  }
+
+  vpc_id = module.vpc.vpc_id
+
+  workers_group_defaults = {
+    root_volume_type = "gp2"
+  }
+
+  worker_groups = [
+    {
+      name                          = "app1-cluster-01"
+      instance_type                 = "t2.small"
+      additional_userdata           = "--kubelet-extra-args --node-labels=cluster=app1,nodegroup=app1-cluster"
+      asg_desired_capacity          = 2
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
+    },
+    {
+      name                          = "app2-cluster-02"
+      instance_type                 = "t2.medium"
+      additional_userdata           = "--kubelet-extra-args --node-labels=cluster=app2,nodegroup=app2-cluster"
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
+      asg_desired_capacity          = 1
+    },
+  ]
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
